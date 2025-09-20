5868144730615debb3e9b01c4097285ed668cd6d
Andrew Carlson <andrewcarlson272@gmail.com>
2025-09-20 11:24:04 -0500

    break: just a save

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
 from flask import *
+import time
 
 app = Flask(__name__)
 
-chatlog = []
+gamePhase = ["lobby", "chat", "guess", "results"]
+current_phase_index = 0
 
 @app.route("/", methods=["GET", "POST"])
-def chat():
-    if request.method == "POST":
-        chatlog.append(request.form['message'])
-    print(chatlog)
-    return render_template("index.html", chatlog=chatlog)
+def getPage():
+    return render_template("index.html")
+
+
+@app.route("/gameState", methods=["GET", "POST"])
+def gameState():
+    global current_phase_index
+
+    if request.method == "GET": # GET: /gameState
+        index = int(time.time() / 5) % len(gamePhase)
+        currentPhase = gamePhase[index]
+
+        data = {
+            "gamePhase": currentPhase
+        }
+        return jsonify(data)
+
+    elif request.method == "POST": # POST: /gameState
+        data = request.get_json()
+        if data.get("nextPhase") == True:
+            current_phase_index = (current_phase_index + 1) % len(gamePhase)
+        print("Received via POST:", data)
+        return jsonify(status="ok")
