5a6021584ce1d34feda569f40676b53382c5b071
Andrew Carlson <andrewcarlson272@gmail.com>
2025-09-22 10:50:08 -0500

    fix: made main.js more readable and broke up functions

--- static/main.js
+++ static/main.js
@@ -1,10 +1,9 @@
 let gameState;
-let chats = ['Apple', 'Banana', 'Oranges']
 let realized_gameState;
-let game_HTML
+let game_HTML;
 
-async function updateGame(){
-	// first update gameState
+async function update_gameState(){
+	// updates the game state on client side
 	try {
 		const response = await fetch('/gameState');
 
@@ -16,52 +15,67 @@ async function updateGame(){
 		console.error('Error fetching data:', error);
 		return;
 	}
+}
 
-	// now update the HTML to show what game phase the room is in
-	if (gameState.gamePhase != realized_gameState ){
-		try {
-			const response = await fetch(`/static/${gameState.gamePhase}.html`);
-			game_HTML = await response.text();
-			realized_gameState = gameState.gamePhase;
-			document.getElementById("game").innerHTML = game_HTML;
-		} catch(error) {
-			console.error('Error fetching html file:', error);
-		}
+async function update_gameFrame(){
+	//renders HTML for each of the phases of the game
+	try {
+		const response = await fetch(`/static/${gameState.gamePhase}.html`);
+		game_HTML = await response.text();
+		realized_gameState = gameState.gamePhase;
+		document.getElementById("game").innerHTML = game_HTML;
+	} catch(error) {
+		console.error('Error fetching html file:', error);
 	}
 
+}
+
+async function lobbyJS(){
 	//if the game is at the lobby stage create a event listener for it to start the game
-	if (gameState.gamePhase == "lobby"){
-	// TODO check if there is already an event listener so that there isn't more added
-		const startButton = document.getElementById("start_game"); 
-		if (startButton && !startButton.dataset.listenerAdded) {
-			startButton.addEventListener('click', function() {	
-				fetch('/gameState', {
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json'
-					},
-					body: JSON.stringify({ nextPhase: true })
-				})
-				.then(res => res.json())
-				.then(data => console.log("POST response:", data));
-			});
-
-			startButton.dataset.listenerAdded = "True";
-		}
+	const startButton = document.getElementById("start_game"); 
+	if (startButton && !startButton.dataset.listenerAdded) {
+		startButton.addEventListener('click', function() {	
+			fetch('/gameState', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({ nextPhase: true })
+			})
+			.then(res => res.json())
+			.then(data => console.log("POST response:", data));
+		});
+		startButton.dataset.listenerAdded = "True";
 	}
-	// if the game si at the chat stage the <ul> should be filled with the chats <li>
-	// TODO: load chats from server side
+
+}
+
+async function chatJS(){
+	// generates the chats on the screen
+	// TODO make the messaging more seemless by adding JS to fetch and send a POST to flask
+	document.getElementById('chat_area').innerHTML = `
+		<ul>
+			${gameState.chats.map(chat => `<li>${chat}</li>`).join('')}
+		</ul>
+		`;
+}
+
+async function updateGame(){
+	update_gameState();
+
+	if (gameState.gamePhase != realized_gameState ){
+		update_gameFrame();
+	}
+
+	//implements JS for each of the phases of the game
+	if (gameState.gamePhase == "lobby"){
+		lobbyJS();
+	}
 	else if (gameState.gamePhase == "chat"){
-	// generates the chats
-	// TODO move this function so that it can be called and updated seperately and quicker	
-		document.getElementById('chat_area').innerHTML = `
-			<ul>
-				${gameState.chats.map(chat => `<li>${chat}</li>`).join('')}
-			</ul>
-			`;
-		}
+		chatJS();
+	}
 }
 
 
-
+//TODO make an async request that won't return until there is a change to the state of the game this will prevent issues that could arise 
 setInterval(updateGame, 1000);
