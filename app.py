afb6d11eaa6c31e88032f9e9e3941cd8b3ce1751
Andrew Carlson <andrewcarlson272@gmail.com>
2025-09-22 10:22:26 -0500

    docs: app.py made to be more readable

--- app.py
+++ app.py
@@ -1,25 +1,45 @@
+"""
+app.py
+
+Web API implemented with flask
+"""
+
 from flask import *
 import time
 
 app = Flask(__name__)
 
+# TODO move this to a sperate file as a sort of "game state object"
 gamePhase = ["lobby", "chat", "guess", "results"]
 chats = []
 current_phase_index = 0
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def getPage():
+    """
+        Packs and sends the frontend to the user.
+
+        Returns:
+            HTML: the rendered index.html file
+    """
     return render_template("index.html")
 
 
 @app.route("/gameState", methods=["GET", "POST"])
 def gameState():
+    """
+        GETs and POSTs to the gameState
+
+        Args: 
+            request: used for method and getting data for post
+
+        Returns:
+            JSON: either returning the status or the gameState
+    """
     global current_phase_index
 
     if request.method == "GET": # GET: /gameState
-        index = int(time.time() / 5) % len(gamePhase)
         currentPhase = gamePhase[current_phase_index]
-
         data = {
             "gamePhase": currentPhase,
             "chats" : chats
@@ -35,14 +55,18 @@ def gameState():
 
 @app.route("/message", methods=["POST"])
 def addMessage():
+    """
+        Receives messages and updates the chatlog
+
+        Args:
+            request: used to obtain the message
+
+        Returns:
+            a redirect to "/" (to be changed)
+    """
+    # TODO will need to be changed to obtain JSON data and return status "OK"
     if request.method == "POST":
         message= request.form.get("message")
         print(f'Message received: {message}')
         chats.append(message)
     return redirect("/")
-
-# Resolves ERROR 404 /favicon.ico not found
-@app.route('/favicon.ico')
-def favicon():
-    return '', 204  # No content
-
