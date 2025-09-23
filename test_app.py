98b149fdc9eb17b5cdd68d1df8b445743702682d
Andrew Carlson <andrewcarlson272@gmail.com>
2025-09-23 09:33:23 -0500

    feat: test '/' in flask complete

--- test_app.py
+++ test_app.py
@@ -1,4 +1,5 @@
 import unittest
+from app import app
 
 class TestUnittest(unittest.TestCase):
     def test_true(self):
@@ -6,3 +7,15 @@ class TestUnittest(unittest.TestCase):
     
     def test_false(self):
         self.assertFalse(False)
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_index(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'<html', response.data)
+
+    
